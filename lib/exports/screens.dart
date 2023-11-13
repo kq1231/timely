2421cdbafaa01d603df663f7f02491a6107d8150
Abu <kifayat639.ku@gmail.com>
2023-11-13 22:00:53 +0500

    Correct file struct

--- lib/exports/screens.dart
+++ lib/exports/screens.dart
@@ -3,4 +3,4 @@ export 'package:timely/modules/tab_4/views/output_screen.dart';
 export 'package:timely/modules/tab_5/views/output_screen.dart';
 export 'package:timely/modules/tab_1/views/output_screen.dart';
 export 'package:timely/modules/home/views/launch_screen.dart';
-export 'package:timely/modules/splash/splash.dart';
+export 'package:timely/modules/splash/views/splash.dart';
