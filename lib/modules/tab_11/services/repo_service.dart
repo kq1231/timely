26bf35bf88e7bc6d4485392bcec4bba6a928e916
Abu <kifayat639.ku@gmail.com>
2023-12-20 08:37:17 +0500

    Cleanness and organization

--- lib/modules/tab_11/services/repo_service.dart
+++ lib/modules/tab_11/services/repo_service.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:timely/modules/common/services/repo_service.dart';
+import 'package:timely/modules/common/notifiers/services/repo_service.dart';
 import 'package:timely/modules/tab_11/models/tab_11_model.dart';
 
 final repositoryServiceProvider =
