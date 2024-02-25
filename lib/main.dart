d609f224431f0f1c79732e9b5eb8c555289e5778
Abu <kifayat639.ku@gmail.com>
2024-02-25 08:09:20 +0500

    Deleted exports folder

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:timely/app_theme.dart';
 import 'package:timely/modules/home/views/tab_buttons.dart';
+import 'package:timely/modules/splash/views/splash.dart';
 import 'package:timely/modules/tab_1/repositories/repo.dart';
 import 'package:timely/reusables.dart';
-import 'exports/screens.dart';
+export 'package:timely/modules/home/views/launch_screen.dart';
+export 'package:timely/modules/splash/views/splash.dart';
+export 'package:timely/modules/tab_1/atomic/pages/fms_page.dart';
+export 'package:timely/modules/tab_1/atomic/pages/tab_1_output_page.dart';
+export 'package:timely/modules/tab_10/atomic/pages/output/tab_10_output_page.dart';
+export 'package:timely/modules/tab_11/atomic/pages/output/tab_11_output_page.dart';
+export 'package:timely/modules/tab_12/atomic/pages/output/tab_12_summary_page.dart';
+export 'package:timely/modules/tab_2/pages/tab_2_output_page.dart';
+export 'package:timely/modules/tab_3/atomic/pages/output/tab_3_output_page.dart';
+export 'package:timely/modules/tab_5/atomic/pages/output/tab_5_output_page.dart';
+export 'package:timely/modules/tab_6/pages/tab_6_output_page.dart';
+export 'package:timely/modules/tab_7/pages/tab_7_output_page.dart';
+export 'package:timely/modules/tab_8/atomic/pages/output/tab_8_output_page.dart';
+export 'package:timely/modules/tab_9/atomic/pages/output/tab_9_summary_page.dart';
 
 void main() {
   runApp(
--- lib/modules/home/views/tab_buttons.dart
+++ lib/modules/home/views/tab_buttons.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timely/app_theme.dart';
-import 'package:timely/exports/screens.dart';
+import 'package:timely/modules/home/views/launch_screen.dart';
 import 'package:timely/modules/home/views/work_in_progress.dart';
+import 'package:timely/modules/tab_1/atomic/pages/tab_1_output_page.dart';
+import 'package:timely/modules/tab_2/pages/tab_2_output_page.dart';
+import 'package:timely/modules/tab_3/atomic/pages/output/tab_3_output_page.dart';
+import 'package:timely/modules/tab_5/atomic/pages/output/tab_5_output_page.dart';
+import 'package:timely/modules/tab_6/pages/tab_6_output_page.dart';
 import 'package:timely/reusables.dart';
 
 final List tabs = [
